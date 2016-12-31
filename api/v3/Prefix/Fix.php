8a97360c9bd9c93f6a79cc5fd84fee11652e5171
Erik Hommel <hommel@ee-atwork.nl>
2016-12-31 15:59:45 +0100

    fix prefix locally tested and approved

--- api/v3/Prefix/Fix.php
+++ api/v3/Prefix/Fix.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * Prefix.Fix API Fix prefix after migration
  *
@@ -11,36 +10,46 @@
  * @throws API_Exception
  */
 function civicrm_api3_prefix_Fix($params) {
-  $returnValues = array("Updated contacts :");
-  // first set the relevant prefix id's in an array (TitelCode / prefix_id)
-  $migratePrefix = array(
-    1 => 1,
-    2 => 2,
-    5 => 3,
-    6 => 4
-  );
-  // get all contact from civicrm with contact type individual and empty prefix
-  $testDAO = CRM_Core_DAO::executeQuery('SELECT * FROM domus_migratie.leden');
-  CRM_Core_Error::debug('test dao', $testDAO);
-  exit();
-
-  $sqlContact = "SELECT id, fist_name, last_name FROM civicrm_contact WHERE contact_type = %1 AND prefix_id IS NULL";
+  $updateCount = 0;
+  // first set the relevant prefix id's in an array (TitelCode / prefix name)
+  $migratePrefixes = array(
+    1 => array('name' => 'Mr.', 'prefix_id' => null, 'label' => null),
+    2 => array('name' => 'Dr.', 'prefix_id' => null, 'label' => null),
+    5 => array('name' => 'Ms.', 'prefix_id' => null, 'label' => null),
+    6 => array('name' => 'Prof. Dr.', 'prefix_id' => null, 'label' => null));
+  // now add prefix ids to the array
+  foreach ($migratePrefixes as $titelCode => $migratePrefix) {
+    $prefix = civicrm_api3('OptionValue', 'getsingle', array(
+      'option_group_id' => 'individual_prefix',
+      'name' => $migratePrefix['name'],
+    ));
+    $migratePrefixes[$titelCode]['prefix_id'] = $prefix['value'];
+    $migratePrefixes[$titelCode]['label'] = $prefix['label'];
+  }
+  // get all contact from civicrm with contact type individual and linked titelcode from leden
+  $sqlContact = "SELECT cc.id, cc.first_name, cc.last_name, ld.TitelCode AS titel_code
+FROM civicrm_contact cc LEFT JOIN domus_migratie.leden ld ON cc.id = ld.LidNummer
+WHERE cc.contact_type = %1 AND ld.TitelCode IS NOT NULL AND ld.TitelCode != '' AND cc.modified_date < CURDATE() LIMIT 500";
   $contact = CRM_Core_DAO::executeQuery($sqlContact, array(1 => array('Individual', 'String')));
-  while ($contact->fetch()) {
-    // get related record from leden with titel
-    $sqlLeden = "SELECT TitelCode FROM leden WHERE LidNummer = %1";
-    $titelCode = CRM_Core_DAO::singleValueQuery($sqlLeden, array(1 => $contact->id, 'Integer'));
-    // now update prefix_id, display_name and addressee_display from contact
-    if (isset($migratePrefix[$titelCode])) {
-      $update = "UPDATE civicrm_contact SET prefix_id = %1, display_name = %2, addressee_display = %2 WHERE id = %3";
-      $displayName = "";
-      $updateParams = array(
-        1 => array($migratePrefix[$titelCode], 'Integer')
-      );
-      CRM_Core_DAO::executeQuery($update, $updateParams);
-      $returnValues[] = $displayName;
+  if ($contact->N == 0) {
+    $returnValues = array('Fixed all individuals');
+  } else {
+    while ($contact->fetch()) {
+      // only if we have a titel code to go from
+      if (isset($migratePrefixes[$contact->titel_code])) {
+        $updateParams = array(
+          'id' => $contact->id,
+          'contact_type' => 'Individual',
+          'display_name' => $migratePrefixes[$contact->titel_code]['label'] . ' ' . $contact->first_name . ' ' . $contact->last_name,
+          'addressee_display' => 1,
+          'email_greeting_id' => 3,
+          'postal_greeting_id' => 3,
+          'prefix_id' => $migratePrefixes[$contact->titel_code]['prefix_id']);
+        civicrm_api3('Contact', 'Create', $updateParams);
+        $updateCount++;
+      }
     }
-
+    $returnValues = array($updateCount . ' contacts fixed');
   }
   return civicrm_api3_create_success($returnValues, $params, 'Prefix', 'Fix');
 }
